d0b1cdbde910974b5eb71870015a4c9ebb2443a2
Serhi Komarichev <komarichev@gmail.com>
2017-12-19 00:35:56 +0200

    change

--- app.js
+++ app.js
@@ -31,39 +31,11 @@ request(url, (err, resp, body) => {
                   "birthday": ""
               });
       });
+
       fs.writeFile('users.json', JSON.stringify(arr), (err) => {
             if (err) throw err;
             console.log('The file has been saved!');
           });
     });
-   // console.log(rdFile);
-//       if (err) {
-//             fs.appendFile("users.json", "[]")
-//       }
-//       console.log(data.toString());
-//       let arr = JSON.parse(data.toString()) || [];
-      
-//           $('tr').each(function(i, elem) {
-//           const childrenLast = $(this).children().last();
-//             const name = childrenLast.prev().text().split(' ');
-//              arr.push({
-//                   "lastname":name[0],
-//                   "firstname":name[1],
-//                   "email":childrenLast.text(),
-//                   "name": `${name[0]} ${name[1]}`,
-//                   "internalPhone": "",
-//                   "position": "",
-//                   "unit": "",
-//                   "department": "",
-//                   "gender": "",
-//                   "img": "",
-//                   "birthday": ""
-//               });
-//     });
-//       console.log(arr);
-//       fs.writeFile('users.json', JSON.stringify(arr), (err) => {
-//             if (err) throw err;
-//             console.log('The file has been saved!');
-//           });
-  
+   
 });
